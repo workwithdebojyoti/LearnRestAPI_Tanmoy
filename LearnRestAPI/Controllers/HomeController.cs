66da7b677abbb74e7cf317868ce7d8c5deecf862
work debojyoti <59592233+workwithdebojyoti@users.noreply.github.com>
2021-03-05 20:19:44 +0530

    Home Controller added.

---
+++ LearnRestAPI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Hello()
+        {
+            return Ok("Hello, from Debojyoti Paul");
+        }
+    }
+}
